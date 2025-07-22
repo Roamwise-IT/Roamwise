73f498c12f83574bc28debbf1167a2b3749f8661
Mitheel Ramdaw <106301219+mitheelramdaw@users.noreply.github.com>
2025-07-22 15:47:16 +0200

    chore : Ryan can you see this ?

--- Frontend/app/(tabs)/index.tsx
+++ Frontend/app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Ryan Can you See this?</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
