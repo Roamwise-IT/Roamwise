c1242818d9a33887e59bc6b7ae885055469d7669
Taegan <taeganks@gmail.com>
2025-08-04 20:52:24 +0200

    fea: updated styling

--- Frontend/app/(tabs)/search.tsx
+++ Frontend/app/(tabs)/search.tsx
@@ -4,12 +4,13 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import TopSearchBar from "../../components/ui/TopSearchBar";
 import Question from "../../components/ui/Question";
 import StoreCard from "../../components/ui/StoreCard";
+import { colors, darkTheme, spacing, margins, padding, borderRadius, borderWidth, sizes, fontFamilies, fontSizes, fontWeights, lineHeights } from "../../constraints";
 
 export default function SearchScreen() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: darkTheme.background}}>
       <TopSearchBar />
-      <ScrollView contentContainerStyle={{ padding: 15 }}>
+      <ScrollView contentContainerStyle={{ padding: padding.lg }}>
         <Question />
         <StoreCard />
         <StoreCard />
--- Frontend/components/ui/WelcomeCard.tsx
+++ Frontend/components/ui/WelcomeCard.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
+import { colors, darkTheme, spacing, margins, padding, borderRadius, borderWidth, sizes, fontFamilies, fontSizes, fontWeights, lineHeights } from "../../constraints";
 
 export default function WelcomeCard({ username, favouriteMall }: { username: string; favouriteMall: string }) {
   return (
-    <View style={{ backgroundColor: "#222", padding: 20, borderRadius: 10 }}>
-      <Text style={{ fontSize: 22, fontWeight: "bold", color: "#fff" }}>WELCOME BACK!</Text>
-      <Text style={{ fontSize: 18, color: "#fff", marginTop: 5 }}>{username}</Text>
-      <Text style={{ fontSize: 14, color: "#aaa", marginTop: 8 }}>
-        Your favourite mall: {favouriteMall}
-      </Text>
+    <View style={{
+       backgroundColor: darkTheme.surface,
+        padding: padding.xl,
+        borderRadius: borderRadius.lg, 
+        flexDirection: "row",
+        alignItems: "center", 
+        borderLeftWidth: 	borderWidth.thin, 
+        borderRightWidth: 	borderWidth.thin,
+        borderBottomWidth: 	borderWidth.thick,
+        borderColor: darkTheme.border.muted,}}>
       <Image
         source={require("../../assets/images/image.png")}
-        style={{ width: 60, height: 60, borderRadius: 30, marginTop: 10 }}
+        style={{ width: 80, height: 80, borderRadius: borderRadius.image, marginRight: margins.topMd, marginLeft: margins.leftOffset }}
       />
+      <View>
+        <Text style={{ fontSize: fontSizes.xxl, fontWeight: "bold", color: darkTheme.text.primary }}>WELCOME BACK!</Text>
+        <Text style={{ fontSize: fontSizes.lg, color:darkTheme.text.primary, marginTop: margins.topSm, textTransform: "uppercase",   fontWeight: "bold",textShadowColor: darkTheme.header.background,textShadowOffset: { width: 2, height: 2 },textShadowRadius: 2,}}>{username}</Text>
+        <Text style={{ fontSize: fontSizes.md, color: darkTheme.text.secondary, marginTop: margins.topMd }}>
+          Favourite Mall: {favouriteMall}
+        </Text>
+      </View>
     </View>
   );
 }
