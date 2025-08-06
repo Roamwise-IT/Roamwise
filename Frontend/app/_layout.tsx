22fe2d502b79b40ddafbc920fa127b3b6b3d9b0e
Mitheel Ramdaw <106301219+mitheelramdaw@users.noreply.github.com>
2025-08-06 02:36:17 +0200

    query gone on mall id search

--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        {/* Hide headers for these routes so we can use our custom HeaderBar */}
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="mall/[mall_id]" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
